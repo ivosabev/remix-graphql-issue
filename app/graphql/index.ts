3c81e2e8a4eee335061a0e023a08e807adec72ba
Ivo Sabev <ivolution@gmail.com>
2022-04-28 14:08:07 +0300

    Remove not needed code

--- app/graphql/index.ts
+++ app/graphql/index.ts
@@ -16,21 +16,3 @@ export const resolvers = {
 
 // export const schema = applyDirectives(makeExecutableSchema({resolvers, typeDefs}), Directives.directives);
 export const schema = makeExecutableSchema({resolvers, typeDefs});
-
-if (process.env.NODE_ENV === 'development') {
-  // Save user readable type system shorthand of schema
-  fs.writeFileSync(`${__dirname}/../schema/schema.graphql`, printSchema(schema));
-
-  // Save JSON of full schema introspection for Babel Relay Plugin to use
-  (async () => {
-    const result = await graphql({schema, source: getIntrospectionQuery()});
-    if (result.errors) {
-      console.error('ERROR introspecting schema: ', JSON.stringify(result.errors, null, 2));
-      process.exit(1);
-    } else {
-      console.log('Rebuild GraphQL Schema...');
-      fs.writeFileSync(`${__dirname}/../schema/schema.json`, JSON.stringify(result, null, 2));
-      // process.exit(0);
-    }
-  })();
-}
